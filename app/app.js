8be6b0b1f1504053286c01be3db5b8569bbed433
girjeshshakya <girjesh01@gmail.com>
2017-12-02 22:25:19 +0530

    changed app.js

--- app/app.js
+++ app/app.js
@@ -1,22 +1,35 @@
-(function(angular) {
-  'use strict';
-angular.
- module('myServiceModule', []).
-  controller('MyController', ['$scope', 'notify', function($scope, notify) {
-    $scope.callNotify = function(msg) {
-      notify(msg);
-    };
-  }]).
- factory('notify', ['$window', function(win) {
-    var msgs = [];
-    return function(msg) {
-      msgs.push(msg);
-      if (msgs.length === 3) {
-        win.alert(msgs.join('\n'));
-        msgs = [];
-      }
-    };
-  }]);
+(function (angular) {
+    'use strict';
+    angular.
+        module('myServiceModule', []).
+        config(function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'login.html'
+                })
+                .when('/dashboard', {
+                    templateUrl: 'dashboard.html'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }).
+        controller('MyController', ['$scope', 'notify', function ($scope, notify) {
+            $scope.callNotify = function (msg) {
+                notify(msg);
+            };
+        }]).
+
+        factory('notify', ['$window', function (win) {
+            var msgs = [];
+            return function (msg) {
+                msgs.push(msg);
+                if (msgs.length === 3) {
+                    win.alert(msgs.join('\n'));
+                    msgs = [];
+                }
+            };
+        }]);
 })(window.angular);
 
 /*
